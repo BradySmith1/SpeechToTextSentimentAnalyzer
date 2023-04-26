a3573c028e792d1c437233ccf3b9d0e7a0724243
Brady Smith <bksmith8@catamount.wcu.edu>
2023-04-26 14:23:51 -0400

    Added implementation for converting from speech to text. Has not been tested yet

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-import sys
+import tkinter.messagebox
 from tkinter import *
 import os
 import sounddevice as sd
 from scipy.io.wavfile import write
 from pydub import AudioSegment
-import time
+from google.cloud import speech
+
+# parameter values for specific numbers needed in the functions below.
+fs = 44100  # Sample rate
+seconds = 3  # Duration of recording
+start_path = "./mp3_files"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './keys/strange-sun-377020-89492c5c249f.json'
+cloud_speech = speech.SpeechClient()
 
 
 def record_window():
@@ -13,19 +20,20 @@ def record_window():
     label.grid(column=0, row=0)
     entry_box = Entry(window)
     entry_box.grid(column=1, row=0)
-    button_ok = Button(window, text="Record", command=lambda: [window.destroy(), record_sound(entry_box.get())])
+    button_ok = Button(window, text="Record", command=lambda: record_sound(entry_box.get(), window))
     button_ok.grid(column=1, row=1)
     button_cancel = Button(window, text="Cancel", command=window.destroy)
     button_cancel.grid(column=0, row=1)
 
 
-def record_sound(file_name):
+def record_sound(file_name, other_window):
     def update():
         my_recording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
         sd.wait()  # Wait until recording is finished
         write('output.wav', fs, my_recording)  # Save as WAV file
         sound = AudioSegment.from_wav('output.wav')
         sound.export(file_name + '.mp3', format='mp3')
+        other_window.destroy()
 
         # Recording sound finishes.
         label.config(text="done.")
@@ -41,10 +49,47 @@ def record_sound(file_name):
     window.mainloop()
 
 
-# parameter values for specific numbers needed in the functions below.
-fs = 44100  # Sample rate
-seconds = 3  # Duration of recording
-start_path = "./mp3_files"
+def get_listbox_selected(listbox_local):
+    selection = []
+    for index in listbox_local.curselection():
+        selection.append(listbox_local.get(index))
+    return selection
+
+
+def speech_analysis(file_name):
+    def update():
+        config_mp3 = speech.RecognitionConfig(
+            sample_rate_hertz=fs,
+            enable_automatic_punctuation=TRUE,
+            language_code='en-US'
+        )
+        with open(file_name + ".mp3", 'rb') as f:
+            byte_data_mp3 = f.read()
+        audio_mp3 = speech.RecognitionAudio(content=byte_data_mp3)
+
+        transcription = cloud_speech.recognize(
+            config=config_mp3,
+            audio=audio_mp3
+        )
+        print(transcription)
+        label.config(text="done.")
+        button_ok = Button(window, text="ok", command=window.destroy)
+        button_ok.grid(column=0, row=1)
+        # with open(file_name + '.txt', 'w') as w:
+        #     w.write(transcription)
+
+    if len(file_name) > 1:
+        tkinter.messagebox.showerror(message="Too many files selected from left panel.")
+        return
+    elif len(file_name) == 0:
+        tkinter.messagebox.showerror(message="No file selected from left panel.")
+        return
+    window = Toplevel()
+    label = Label(window, text="Transcribing in progress...")
+    label.grid(column=0, row=0)
+    window.after(1000, update)
+    window.mainloop()
+
 
 # Populating of the directory with possible mp3 files
 directories = os.listdir(start_path)
@@ -78,7 +123,7 @@ title = Label(options_frame, text="Sentiment Analyzer")
 title.grid(row=0, column=1, pady=125, sticky="n")
 button1 = Button(options_frame, text="New Record", command=record_window)
 button1.grid(row=1, column=1, pady=25, sticky="n")
-button2 = Button(options_frame, text="Transcribe")
+button2 = Button(options_frame, text="Transcribe", command=lambda: speech_analysis(get_listbox_selected(listbox)))
 button2.grid(row=2, column=1, pady=25, sticky="n")
 button3 = Button(options_frame, text="Perform\n Sentiment\n Analysis")
 button3.grid(row=3, column=1, pady=25, sticky="n")
